6efc30aabaf392d71cc7d4c64e57c7be308d02eb
SongLi3 <43899053+SongLi3@users.noreply.github.com>
2018-12-09 17:17:10 -0500

    Add files via upload

---
+++ iot-gateway finall/src/main/java/org/songli/iot/common/ConfigConst.java
@@ -0,0 +1,17 @@
+package org.songli.iot.common;
+public class ConfigConst{
+//mqtt	
+public static String DEFAULT_MQTT_PROTOCOL= "tcp";
+public static String DEFAULT_MQTT_SERVER = "test.mosquitto.org";
+public static int DEFAULT_MQTT_PORT = 1883;
+//defaultQos = 2
+
+
+
+
+// Coap
+public static String SECURE_COAP_PROTOCOL = "coap";
+public static String DEFAULT_COAP_SERVER = "localhost";
+public static int SECURE_COAP_PORT = 5683;
+
+}
